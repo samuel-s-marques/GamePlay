760f319af74d9bfa87058a98219b4fd22fb63a91
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-06-25 00:24:59 -0300

    Update index.tsx

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,13 +5,16 @@ import { CategorySelect } from '../../components/CategorySelect';
 import { ListHeader } from '../../components/ListHeader';
 import { Appointment } from '../../components/Appointment';
 import { ListDivider } from '../../components/ListDivider';
-
+import { Background } from '../../components/Background';
 import { Profile } from '../../components/Profile';
 
 import { styles } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 export function Home() {
 	const [category, setCategory] = useState('');
+
+	const navigation = useNavigation();
 
 	const appointment = [
 		{
@@ -45,11 +48,19 @@ export function Home() {
 		categoryId === category ? setCategory('') : setCategory(categoryId);
 	}
 
+	function handleAppointmentDetails() {
+		navigation.navigate('AppointmentDetails')
+	}
+
+	function handleAppointmentCreate() {
+		navigation.navigate('AppointmentCreate')
+	}
+
 	return (
-		<View>
+		<Background>
 			<View style={styles.header}>
 				<Profile />
-				<ButtonAdd/>
+				<ButtonAdd onPress={handleAppointmentCreate}/>
 			</View>
 
 			<CategorySelect
@@ -67,13 +78,16 @@ export function Home() {
 					data={appointment}
 					keyExtractor={item => item.id}
 					renderItem={({item}) => (
-						<Appointment data={item}/>
+						<Appointment 
+							data={item}
+							onPress={handleAppointmentDetails}
+						/>
 					)}
 					ItemSeparatorComponent={() => <ListDivider/>}
 					style={styles.matches}
 					showsVerticalScrollIndicator={false}
 				/>
 			</View>
-		</View>
+		</Background>
 	);
 }
