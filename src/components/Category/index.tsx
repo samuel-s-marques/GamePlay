fd7391ff8c5aa4b4d410e3cbf50bdf4e5fa53901
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-06-24 23:55:53 -0300

    improved category

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -9,6 +9,7 @@ import { View, Text } from 'react-native';
 
 type Props = RectButtonProps & {
 	title: string;
+	hasCheckBox?: boolean;
 	icon: React.FC<SvgProps>;
 	checked?: boolean;
 }
@@ -17,20 +18,29 @@ export function Category({
 	title,
 	icon: Icon,
 	checked = false,
+	hasCheckBox = false,
 	...rest
 }: Props) {
+	const {secondary40, secondary50, secondary70, secondary75 } = theme.colors;
+
 	return (
 		<RectButton {...rest}>
-			<LinearGradient style={styles.container} colors={[theme.colors.secondary50, theme.colors.secondary70]}>
-				<View style={[styles.content, {opacity: checked ? 1 : 0.4}]}>
-					<View style={checked ? styles.checked : styles.check}/>
+			<LinearGradient style={styles.container} colors={[secondary50, secondary70]}>
+				<LinearGradient 
+					style={[styles.content, {opacity: checked ? 1 : 0.5}]}
+					colors={[checked ? secondary75 : secondary50, secondary40]}
+				>
+					{
+						hasCheckBox &&
+						<View style={checked ? styles.checked : styles.check}/>
+					}
 
 					<Icon width={48} height={48}/>
 
 					<Text style={styles.title}>
 						{ title }
 					</Text>
-				</View>
+				</LinearGradient>
 			</LinearGradient>
 		</RectButton>
 	)
