4efd85ab7b68c26482f561fd1901b835346ace7a
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-06-25 00:25:01 -0300

    Update index.tsx

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,6 +3,7 @@ import { View, Image, Text } from 'react-native';
 
 import { styles } from './styles';
 import { ButtonIcon } from '../../components/ButtonIcon/';
+import { Background } from '../../components/Background';
 import IllustrationImg from '../../assets/illustration.png';
 import { useNavigation } from '@react-navigation/native';
 
@@ -14,27 +15,29 @@ export function SignIn() {
 	}
 
 	return (
-		<View style={styles.container}>
-			<Image 
-				source={IllustrationImg} 
-				style={styles.image} 
-				resizeMode="stretch"
-			/>
-		
-			<View style={styles.content}>
-				<Text style={styles.title}>
-					Conecte-se {'\n'}
-					e organize suas {'\n'}
-					jogatinas
-				</Text>
+		<Background>
+			<View style={styles.container}>
+				<Image 
+					source={IllustrationImg} 
+					style={styles.image} 
+					resizeMode="stretch"
+				/>
+			
+				<View style={styles.content}>
+					<Text style={styles.title}>
+						Conecte-se {'\n'}
+						e organize suas {'\n'}
+						jogatinas
+					</Text>
 
-				<Text style={styles.subtitle}>
-					Crie grupos para jogar seus games {'\n'}
-					favorito com seus amigos
-				</Text>
+					<Text style={styles.subtitle}>
+						Crie grupos para jogar seus games {'\n'}
+						favorito com seus amigos
+					</Text>
 
-				<ButtonIcon title="Entrar com Discord" onPress={handleSignIn}/>
+					<ButtonIcon title="Entrar com Discord" onPress={handleSignIn}/>
+				</View>
 			</View>
-		</View>
+		</Background>
 	);
 }
