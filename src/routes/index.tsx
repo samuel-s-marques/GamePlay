0e665ed2d781be68e507615e297d5c256f63202a
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-06-25 00:23:31 -0300

    update routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { AuthRoutes } from './auth.routes';
-import { Background } from '../components/Background';
 
 export function Routes() {
 	return (
