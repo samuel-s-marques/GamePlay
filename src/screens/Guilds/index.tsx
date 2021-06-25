304948a3101fb7f82fdfa8f8281a032b8de48629
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-06-24 23:56:49 -0300

    add guilds

---
+++ src/screens/Guilds/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, FlatList } from 'react-native';
+import { Guild, GuildProps } from '../../components/Guild';
+import { ListDivider } from '../../components/ListDivider';
+
+import { styles } from './styles';
+
+type Props = {
+	handleGuildSelect: (guild: GuildProps) => void;
+}
+
+export function Guilds({handleGuildSelect}: Props) {
+	const guilds = [
+		{
+			id: '1',
+			name: 'Lendários',
+			icon: 'image.jpg',
+			owner: true,
+		},
+		{
+			id: '2',
+			name: 'Wowzinho',
+			icon: 'image.jpg',
+			owner: false,
+		}
+	]
+
+	return (
+		<View style={styles.container}>
+			<FlatList 
+				data={guilds}
+				keyExtractor={item => item.id}
+				renderItem={({item}) => (
+					<Guild 
+						onPress={() => handleGuildSelect(item)}
+						data={item} 
+					/>
+				)}
+				showsVerticalScrollIndicator={false}
+				ItemSeparatorComponent={() => <ListDivider />}
+				style={styles.guilds}
+			/>
+		</View>
+	)
+}
