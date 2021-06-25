4ff7fb034b1866540492aae0ddc69ce4b0f7f9dc
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-06-24 23:57:22 -0300

    update buttonicon

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -15,7 +15,8 @@ export const styles = StyleSheet.create({
 		flex: 1,
 		color: theme.colors.heading,
 		fontSize: 15,
-		textAlign: 'center'
+		textAlign: 'center',
+		fontFamily: theme.fonts.text500,
 	},
 
 	iconWrapper: {
